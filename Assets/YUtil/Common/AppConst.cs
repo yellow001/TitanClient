ce9f36f7a96a3ba63509bc800c0b8a8cc160ebfd
志中 梁 <1144916875@qq.com>
2018-01-19 14:20:26 +0800

    YUtil 目录存放大更改

---
+++ Assets/YUtil/Common/AppConst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public static class AppConst{
+    
+    //public static readonly string AbPath = Application.streamingAssetsPath;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    public static string ResPath = Application.persistentDataPath;
+#elif UNITY_STANDALONE || UNITY_EDITOR
+    public static string ResPath = Application.streamingAssetsPath;
+#endif
+
+    public static readonly bool UpdateMode = false;         //是否更新资源
+    public static readonly bool LuaAssetMode = true;       //是否从资源中加载lua脚本
+    public static readonly string LuaPath = Application.dataPath + "/RideClient/Res/lua/";  //调试模式下lua脚本存放路径
+    public static readonly string connKey = "rideClient";
+
+    public static readonly string gateIP = "127.0.0.1";
+    //public static readonly string gateIP = "127.0.0.1";
+    public static readonly int gatePort = 12345;
+
+    public static readonly string[] dontPack = new string[] { ".cs", ".meta" };
+
+    public static readonly string httpUrl = "http://localhost:7777/";
+    
+
+    public static string AssetEx = ".bundle";
+
+}
