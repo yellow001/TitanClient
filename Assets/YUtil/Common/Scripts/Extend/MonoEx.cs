b4ff437b797a3c0d47271b73c36d444b2433052b
志中 梁 <1144916875@qq.com>
2018-01-19 17:36:04 +0800

    迁移部分代码避免错误

--- Assets/YUtil/Common/Scripts/Extend/MonoEx.cs
+++ Assets/YUtil/Common/Scripts/Extend/MonoEx.cs
@@ -5,25 +5,6 @@ using UnityEngine;
 
 public static class MonoEx{
 
-    #region UI弹窗
-    public static void AddMsg(this MonoBehaviour mono, string msg, Action okDe = null, Action cancleDe = null) {
-        AlertMgr.Ins.AddMsg(new MsgUIModel(msg, "消息", okDe, cancleDe));
-    }
-
-    public static void AddMsg(this MonoBehaviour mono, MsgUIModel m) {
-        AlertMgr.Ins.AddMsg(m);
-    }
-
-    public static void AddTip(this MonoBehaviour mono, string msg, float time = 2.5f) {
-        //YManager.Ins.addTipModel(new TipModel(msg,time));
-        AlertMgr.Ins.AddTip(new TipUIModel(msg, time));
-    }
-
-    public static void AddTip(this MonoBehaviour mono, TipUIModel m) {
-        AlertMgr.Ins.AddTip(m);
-    }
-    #endregion
-
     #region 时间事件
     public static void AddTimeEvent(this MonoBehaviour mono, TimeEvent model)
     {
---
+++ Assets/YUtil/Tools/UGUIScriptCreator/Scripts/UIMonoEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UIMonoEx  {
+    #region UI弹窗
+    public static void AddMsg(this MonoBehaviour mono, string msg, Action okDe = null, Action cancleDe = null) {
+        AlertMgr.Ins.AddMsg(new MsgUIModel(msg, "消息", okDe, cancleDe));
+    }
+
+    public static void AddMsg(this MonoBehaviour mono, MsgUIModel m) {
+        AlertMgr.Ins.AddMsg(m);
+    }
+
+    public static void AddTip(this MonoBehaviour mono, string msg, float time = 2.5f) {
+        //YManager.Ins.addTipModel(new TipModel(msg,time));
+        AlertMgr.Ins.AddTip(new TipUIModel(msg, time));
+    }
+
+    public static void AddTip(this MonoBehaviour mono, TipUIModel m) {
+        AlertMgr.Ins.AddTip(m);
+    }
+    #endregion
+}
