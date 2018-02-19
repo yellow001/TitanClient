1a5344c804b6273b98b222a432873f37e8e37a0e
yellow <1144916875@qq.com>
2018-02-19 08:55:52 +0800

    添加kuly资源与dynamicbone

---
+++ Assets/_Kuly/Scripts/SyncBlendShape.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+[ExecuteInEditMode]
+public class SyncBlendShape : MonoBehaviour {
+    SkinnedMeshRenderer m_currentRender;
+    public SkinnedMeshRenderer targetRender;
+    public int currentIndex;
+    public int targetIndex;
+	// Use this for initialization
+	void Start () {
+        m_currentRender = GetComponent<SkinnedMeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetRender == null) { return; }
+        if (m_currentRender == null) { m_currentRender = GetComponent<SkinnedMeshRenderer>(); }
+
+        m_currentRender.SetBlendShapeWeight(currentIndex, targetRender.GetBlendShapeWeight(targetIndex));
+
+	}
+}
