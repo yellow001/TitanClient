dae7b25c7022cb9672ee078497eb13d01a43b339
yellow <1144916875@qq.com>
2018-02-23 21:57:50 +0800

    场景资源

---
+++ Assets/_Kuly/Scripts/AssetList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AssetList", menuName = "YUtil/AssetList", order = 1)]
+[System.Serializable]
+public class AssetList : ScriptableObject {
+    public List<string> list;
+}
