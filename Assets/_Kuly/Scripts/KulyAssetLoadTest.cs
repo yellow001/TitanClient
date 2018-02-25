c1fc5e2642c5327e3a31930b6155e70ffaeeaae7
yellow <1144916875@qq.com>
2018-02-25 21:35:27 +0800

    模型+贴图修改

--- Assets/_Kuly/Scripts/KulyAssetLoadTest.cs
+++ Assets/_Kuly/Scripts/KulyAssetLoadTest.cs
@@ -8,10 +8,12 @@ public class KulyAssetLoadTest : MonoBehaviour {
 
     public KulyHairCtrl hairCtrl;
 
+
+    public string hairID="hair0";
 	// Use this for initialization
 	void Start () {
         if (hairCtrl != null)
-            hairCtrl.RefreshHair("hair0");
+            hairCtrl.RefreshHair(hairID);
 
         if (clothCtrl!=null)
             clothCtrl.RefreshCloth("cloth0", new string[] { "#000000", "#ffffff" });
